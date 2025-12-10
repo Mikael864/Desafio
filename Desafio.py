4c04089a9fba24656134e370e61304f04843dc5e
Mikael Carneiro <244967838+Mikael864@users.noreply.github.com>
2025-12-10 11:25:11 +0000

    Apresentação de todas as funções matemáticas

--- Desafio.py
+++ Desafio.py
@@ -1,8 +1,18 @@
 
 #numero1=input("Qual é o numero que quer solicitar")
 #numero2=input("Qual é o segundo numero que quer solicitar")
+#Apresentação de todas as funções matemáticas
 numero1=10
 numero2=8
 
-resultado=numero1*numero2
-print(resultado)
+multiplicação=numero1*numero2
+print("A multiplicação é" , multiplicação)
+
+soma=numero1+numero2
+print("A soma é" , soma)
+
+Subtração=numero1-numero2
+print("A Subtração é" , Subtração)
+
+divisão=numero1/numero2
+print("A divisão é" , divisão)
