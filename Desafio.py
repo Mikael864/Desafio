6c6f6438e01f853c701790f4eae84ddf5bf2001a
Mikael Carneiro <244967838+Mikael864@users.noreply.github.com>
2025-12-10 11:25:44 +0000

    correção da divisão

--- Desafio.py
+++ Desafio.py
@@ -5,14 +5,5 @@
 numero1=10
 numero2=8
 
-multiplicação=numero1*numero2
-print("A multiplicação é" , multiplicação)
-
-soma=numero1+numero2
-print("A soma é" , soma)
-
-Subtração=numero1-numero2
-print("A Subtração é" , Subtração)
-
 divisão=numero1/numero2
 print("A divisão é" , divisão)
