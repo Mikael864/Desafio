cf3648139693992fb91429e70cdffb21bb25f081
Mikael Carneiro <244967838+Mikael864@users.noreply.github.com>
2025-12-10 11:02:55 +0000

    soma dos numeros

--- Desafio.py
+++ Desafio.py
@@ -1,3 +1,8 @@
-numero1=input("Qual é o numero que quer solicitar")
-numero2=input("Qual é o segundo numero que quer solicitar")
 
+#numero1=input("Qual é o numero que quer solicitar")
+#numero2=input("Qual é o segundo numero que quer solicitar")
+numero1=10
+numero2=8
+
+resultado=numero1+numero2
+print(resultado)
